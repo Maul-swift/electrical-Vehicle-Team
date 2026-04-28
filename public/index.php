40d875bb0b0adb947cad0b8d2335233a872f3e97
Youpiiiiiiiiiiii <yudannuralif@gmail.com>
2026-04-28 21:24:54 +0700

    feat: initial project structure

---
+++ public/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<!-- MAUL -->
+<nav>
+    <div>Logo</div>
+    <div></div>
+    <a href=""></a>
+</nav>
+
+<section class="home">
+     
+</section>
+
+
+<!-- AJIIIIIII -->
+    
+</body>
+</html>
